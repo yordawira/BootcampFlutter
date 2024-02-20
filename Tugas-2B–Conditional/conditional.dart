af2d26875335fabc639ae578c5808c86e4b2a848
yordawira <wiraaswin275@gmail.com>
2024-02-20 20:54:18 +0700

    feat master : add question 2a -2

--- Tugas-2B–Conditional/conditional.dart
+++ Tugas-2B–Conditional/conditional.dart
@@ -1,4 +1,7 @@
-import 'dart:io';
+
+/*import 'dart:io';
+
+1. Ternary operator 
 
 void main() {
   stdout.write('Apakah Anda ingin menginstall aplikasi? (Y/T): ');
@@ -8,5 +11,45 @@ void main() {
   String message = answer == 'Y' ? 'Anda akan menginstall aplikasi Dart' : 'Aborted';
   
   print(message);
+
+}*/
+
+import 'dart:io';
+
+void main() {
+  
+  stdout.write('Masukkan nama Anda: ');
+  String nama = stdin.readLineSync()!;
+  
+  
+  stdout.write('Pilih peran Anda (Penyihir, Guard, atau Werewolf): ');
+  String peran = stdin.readLineSync()!.toLowerCase(); 
+
+  
+  if (nama.isEmpty) {
+    print('Nama harus diisi!');
+  }
+  
+  else if (peran.isEmpty) {
+    print('Halo $nama, Pilih peranmu untuk memulai game!');
+  }
+  
+  else {
+    print('Selamat datang di Dunia Werewolf, $nama');
+    
+    switch (peran) {
+      case 'penyihir':
+        print('Halo Penyihir $nama, kamu dapat melihat siapa yang menjadi werewolf!');
+        break;
+      case 'guard':
+        print('Halo Guard $nama, kamu akan membantu melindungi temanmu dari serangan werewolf.');
+        break;
+      case 'werewolf':
+        print('Halo Werewolf $nama, Kamu akan memakan mangsa setiap malam!');
+        break;
+      default:
+        print('Peran yang Anda pilih tidak valid.');
+    }
+  }
 }
-//1. Ternary operator 
+
