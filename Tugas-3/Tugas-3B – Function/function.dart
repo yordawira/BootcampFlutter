e4a5a4e7168c36b24a612b9f66d74d8c76019b5e
yordawira <wiraaswin275@gmail.com>
2024-02-21 18:20:35 +0700

    feat master : add questions 3 - 3B number 2

--- Tugas-3/Tugas-3B – Function/function.dart
+++ Tugas-3/Tugas-3B – Function/function.dart
@@ -1,9 +1,30 @@
 // No. 1 
 
-String teriak() {
-  return "Halo Sanbers!";
+//String teriak() {
+  //return "Halo Sanbers!";
+//}
+
+//void main() {
+  //print(teriak()); 
+//}
+
+
+
+
+
+
+
+
+// No. 2 
+
+int kalikan(int num1, int num2) {
+  return num1 * num2;
 }
 
 void main() {
-  print(teriak()); // Output: "Halo Sanbers!"
+  var num1 = 12;
+  var num2 = 4;
+
+  var hasilKali = kalikan(num1, num2);
+  print(hasilKali); 
 }
