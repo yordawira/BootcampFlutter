da229aa44ccdadddefdb110aef46698002a7cfec
yordawira <wiraaswin275@gmail.com>
2024-02-21 18:27:01 +0700

    feat master : add questions 3 - 3B number 3

--- Tugas-3/Tugas-3B – Function/function.dart
+++ Tugas-3/Tugas-3B – Function/function.dart
@@ -15,16 +15,42 @@
 
 
 
+
 // No. 2 
 
-int kalikan(int num1, int num2) {
-  return num1 * num2;
+//int kalikan(int num1, int num2) {
+ // return num1 * num2;
+//}
+
+//void main() {
+ // var num1 = 12;
+//  var num2 = 4;
+
+  //var hasilKali = kalikan(num1, num2);
+  //print(hasilKali); 
+//}
+
+
+
+
+
+
+
+
+
+// No. 3
+
+String introduce(String name, int age, String address, String hobby) {
+  return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
 }
 
 void main() {
-  var num1 = 12;
-  var num2 = 4;
+  final String name = "Achmad";
+  final int age = 30;
+  final String address = "Jln. Malioboro, Yogyakarta";
+  final String hobby = "Gaming";
 
-  var hasilKali = kalikan(num1, num2);
-  print(hasilKali); 
+  final perkenalan = introduce(name, age, address, hobby);
+  print(perkenalan); 
 }
+
