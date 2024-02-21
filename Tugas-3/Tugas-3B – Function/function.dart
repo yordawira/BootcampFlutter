0604f3e919d078f3f4378d1258b70a24d830c4e6
yordawira <wiraaswin275@gmail.com>
2024-02-21 18:34:15 +0700

    feat master : add questions 3 - 3B number 4

--- Tugas-3/Tugas-3B – Function/function.dart
+++ Tugas-3/Tugas-3B – Function/function.dart
@@ -40,17 +40,48 @@
 
 // No. 3
 
-String introduce(String name, int age, String address, String hobby) {
-  return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+//String introduce(String name, int age, String address, String hobby) {
+  //return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+//}
+
+//void main() {
+  //final String name = "Achmad";
+  //final int age = 30;
+  //final String address = "Jln. Malioboro, Yogyakarta";
+  //final String hobby = "Gaming";
+
+  //final perkenalan = introduce(name, age, address, hobby);
+  //print(perkenalan); 
+//}
+
+
+
+
+
+
+
+
+
+
+// No.4
+
+int factorial(int n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
 }
 
 void main() {
-  final String name = "Achmad";
-  final int age = 30;
-  final String address = "Jln. Malioboro, Yogyakarta";
-  final String hobby = "Gaming";
 
-  final perkenalan = introduce(name, age, address, hobby);
-  print(perkenalan); 
+  int number = 6;
+  int result = factorial(number);
+  print("$number! = $result"); // Output: 6! = 720
+  
 }
 
